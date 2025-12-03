5e99c5867a502cbf4155861be7e6fbf11675e430
just2BeeAndrew <andrew.dudal.1997@gmail.com>
2025-12-03 22:26:56 +0300

    add sandbox methods

--- src/modules/testing/api/testing.controller.ts
+++ src/modules/testing/api/testing.controller.ts
@@ -1,17 +1,35 @@
-import { Controller, Delete, HttpCode } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { CommandBus } from '@nestjs/cqrs';
 import { DeleteAllCommand } from '../application/usecases/delete-all.usecase';
 
 @Controller('testing')
 export class TestingController {
-  constructor(
-    private commandBus: CommandBus,
-  ) {
-  }
+  constructor(private commandBus: CommandBus) {}
+
+  @Get('sandbox')
+  @HttpCode(HttpStatus.OK)
+  async getSandbox() {}
+
+  @Post('sandbox')
+  @HttpCode(HttpStatus.OK)
+  async postSandbox(){}
+
+  @Put('sandbox')
+  @HttpCode(HttpStatus.OK)
+  async putSandbox() {}
+
 
   @Delete('all-data')
-  @HttpCode(204)
-  async deleteAll(){
-    return this.commandBus.execute<DeleteAllCommand>(new DeleteAllCommand())
+  @HttpCode(HttpStatus.OK)
+  async deleteAll() {
+    return this.commandBus.execute<DeleteAllCommand>(new DeleteAllCommand());
   }
 }
